5d9c4bb1d8223a74803940d3df2e252cef5e2934
Ivo Zitnik <xzitnik1@aisa.fi.muni.cz>
2025-02-24 22:53:48 +0100

    Implemented all the functions

--- src/Hogwarts.hs
+++ src/Hogwarts.hs
@@ -5,7 +5,8 @@
 module Hogwarts (getDB, getActor, getPatronus, getMugglebornNames, getHouseCounts) where
 
 import Data.Aeson
-import Data.ByteString.Lazy
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as LB
 import Data.Map (Map)
 import qualified Data.Map as M
 import GHC.Generics
@@ -32,6 +33,24 @@ instance FromJSON Person where
     house <- o .: "house"
     return Person {..}
 
+personName :: Person -> String
+personName (Person {..}) = name
+
+personPatronus :: Person -> String
+personPatronus (Person {..}) = patronus
+
+personActor :: Person -> String
+personActor (Person {..}) = actor
+
+personHouse :: Person -> String
+personHouse (Person {..}) = house
+
+-- | vrátí zdali je postava z mudlovské rodiny
+-- v ukázkovém výstupu getMuggleborn je 'Lilly Potter' jež je half-blood
+isMuggle :: Person -> Bool
+isMuggle (Person {..}) = bornType == "muggleborn" || bornType == "half-blood"
+
+-- | json url
 base_url :: String
 base_url = "https://www.fi.muni.cz/~xjonas/hpcharacters.json"
 
@@ -43,12 +62,17 @@ fetchJSON = do
     then pure $ Left (show . statusMessage $ getResponseStatus response)
     else do
       let result = getResponseBody response
-      pure (Right $ fromStrict result)
+      pure (Right $ LB.fromStrict result) -- TODO: Upravit importy aby nebylo třeba convertovat z Lazy na strict
 
+-- | Interní funkce pro konverzi z listu postav na mapu
+-- kde klíč je jméno postavy a hodnotou postava
 parseToDB :: [Person] -> Map String Person
-parseToDB = undefined
+parseToDB = foldl go M.empty
+  where
+    go :: Map String Person -> Person -> Map String Person
+    go b a = M.insert (personName a) a b
 
--- | Returns database of harry potter characters or error
+-- | Vrátí databází postav nebo string s chybou
 getDB :: IO (Either String DB)
 getDB = do
   res <- fetchJSON
@@ -59,14 +83,32 @@ getDB = do
         (Right out) -> pure $ Right (parseToDB out)
         (Left err) -> pure $ Left err
 
+-- | Vrátí herce pro danou postavu
 getActor :: String -> DB -> Maybe String
-getActor = undefined
+getActor key = maybe Nothing (Just . personActor) . M.lookup key
 
+-- | Vrátí patrona pro danou postavu
 getPatronus :: String -> DB -> Maybe String
-getPatronus = undefined
+getPatronus key = maybe Nothing (Just . personPatronus) . M.lookup key
 
+-- | Vrátí všechny postavy z mudlovské rodiny
 getMugglebornNames :: DB -> [String]
-getMugglebornNames = undefined
+getMugglebornNames = foldl go []
+  where
+    go :: [String] -> Person -> [String]
+    go b a
+      | isMuggle a = personName a : b
+      | otherwise = b
 
+-- | Vrátí list dvojic (jméno koleje, počet studentů)
 getHouseCounts :: DB -> [(String, Int)]
-getHouseCounts = undefined
+getHouseCounts = M.toList . foldr go M.empty
+  where
+    go :: Person -> Map String Int -> Map String Int
+    go p =
+      let house = personHouse p
+       in if house /= ""
+            then M.alter msucc house
+            else id
+    msucc :: Maybe Int -> Maybe Int
+    msucc = Just . maybe 1 (+ 1)
